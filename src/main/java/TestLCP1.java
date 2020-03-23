ad1af99a2a223513ce55d68c1c7f525a6611632e
wmlia <44492779+wmlia@users.noreply.github.com>
2020-03-23 18:54:40 +0800

    Update TestLCP1.java

--- src/main/java/TestLCP1.java
+++ src/main/java/TestLCP1.java
@@ -1,4 +1,4 @@
-public class TestLCP {
+public class TestLCP1 {
     public static void main(String[] args) {
 //        LCP 1. 猜数字
 //        小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个，小A 每次也从 1, 2, 3 中选择一个猜。他们一共进行三次这个游戏，请返回 小A 猜对了几次？
